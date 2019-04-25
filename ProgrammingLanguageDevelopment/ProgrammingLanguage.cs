a2eb7569b86676ab180e481b32fdb04a5b0d5663
szymonzuk <32895733+szymonzuk@users.noreply.github.com>
2019-04-25 17:03:10 +0200

    Add files via upload

---
+++ ProgrammingLanguageDevelopment/ProgrammingLanguage.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgrammingLanguageDevelopment
+{
+    class ProgrammingLanguage
+    {
+        public int Year;            //first appeared
+        public string Paradigm;     //imperative, structured, object-oriented or multi-paradigm 
+        public string Typing;       //typing discipline: static or dynamic
+        public string Level;        //high-level or low-level
+    }
+}
