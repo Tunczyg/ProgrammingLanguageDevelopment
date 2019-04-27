f60e2ea34f73ae70d1c26188ebb1d8832f2a7069
ilonatommy <ili.ili@interia.pl>
2019-04-27 17:53:50 +0200

    Minor refactoing + added .gitignore for VS.

---
+++ ProgrammingLanguageDevelopment/DataProvider.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProgrammingLanguageDevelopment
+{
+    class DataProvider
+    {
+        public List<ProgrammingLanguage> GetLanguageFeaturesStaticData()
+        {
+            //from most to least popular according to TIOBE (23rd march 2019)
+            return new List<ProgrammingLanguage>() {
+                new ProgrammingLanguage("java", 1995, ProgrammingLanguage.Paradigm.Multi_paradigm, ProgrammingLanguage.Typing.Static, ProgrammingLanguage.Level.High),
+                new ProgrammingLanguage("c", 1972, ProgrammingLanguage.Paradigm.Structured, ProgrammingLanguage.Typing.Static, ProgrammingLanguage.Level.High),
+                new ProgrammingLanguage("python", 1990, ProgrammingLanguage.Paradigm.Multi_paradigm, ProgrammingLanguage.Typing.Dynamic, ProgrammingLanguage.Level.High),
+                new ProgrammingLanguage("c++", 1983, ProgrammingLanguage.Paradigm.Multi_paradigm, ProgrammingLanguage.Typing.Static, ProgrammingLanguage.Level.High),
+                new ProgrammingLanguage("visualBasicNET", 2001, ProgrammingLanguage.Paradigm.Multi_paradigm, ProgrammingLanguage.Typing.Static, ProgrammingLanguage.Level.High),
+                new ProgrammingLanguage("c#", 2000, ProgrammingLanguage.Paradigm.Multi_paradigm, ProgrammingLanguage.Typing.Dynamic, ProgrammingLanguage.Level.High),
+                new ProgrammingLanguage("javaScript", 1995, ProgrammingLanguage.Paradigm.Multi_paradigm, ProgrammingLanguage.Typing.Dynamic, ProgrammingLanguage.Level.High),
+                new ProgrammingLanguage("php", 1995, ProgrammingLanguage.Paradigm.Multi_paradigm, ProgrammingLanguage.Typing.Dynamic, ProgrammingLanguage.Level.High),
+                new ProgrammingLanguage("sql", 1995, ProgrammingLanguage.Paradigm.Multi_paradigm, ProgrammingLanguage.Typing.Static, ProgrammingLanguage.Level.High),
+                new ProgrammingLanguage("objectiveC", 1983, ProgrammingLanguage.Paradigm.Multi_paradigm, ProgrammingLanguage.Typing.Dynamic, ProgrammingLanguage.Level.High),
+                new ProgrammingLanguage("matlab", 1980, ProgrammingLanguage.Paradigm.Multi_paradigm, ProgrammingLanguage.Typing.Dynamic, ProgrammingLanguage.Level.High),
+                new ProgrammingLanguage("assembly", 1949, ProgrammingLanguage.Paradigm.Imperative, ProgrammingLanguage.Typing.Static, ProgrammingLanguage.Level.Low),
+                new ProgrammingLanguage("perl", 1987, ProgrammingLanguage.Paradigm.Multi_paradigm, ProgrammingLanguage.Typing.Dynamic, ProgrammingLanguage.Level.High),
+                new ProgrammingLanguage("r", 1993, ProgrammingLanguage.Paradigm.Multi_paradigm, ProgrammingLanguage.Typing.Dynamic, ProgrammingLanguage.Level.High),
+                new ProgrammingLanguage("ruby", 1995, ProgrammingLanguage.Paradigm.Multi_paradigm, ProgrammingLanguage.Typing.Dynamic, ProgrammingLanguage.Level.High),
+                new ProgrammingLanguage("groovy", 2003, ProgrammingLanguage.Paradigm.Object_oriented, ProgrammingLanguage.Typing.Dynamic, ProgrammingLanguage.Level.High),
+                new ProgrammingLanguage("swift", 2014, ProgrammingLanguage.Paradigm.Multi_paradigm, ProgrammingLanguage.Typing.Static, ProgrammingLanguage.Level.High),
+                new ProgrammingLanguage("go", 2009, ProgrammingLanguage.Paradigm.Multi_paradigm, ProgrammingLanguage.Typing.Static, ProgrammingLanguage.Level.High),
+                new ProgrammingLanguage("objectPascal", 1986, ProgrammingLanguage.Paradigm.Multi_paradigm, ProgrammingLanguage.Typing.Dynamic, ProgrammingLanguage.Level.Low),
+                new ProgrammingLanguage("visualBasic", 1991, ProgrammingLanguage.Paradigm.Object_oriented, ProgrammingLanguage.Typing.Static, ProgrammingLanguage.Level.High),
+            };
+        }
+
+        //todo:
+        public List<ProgrammingLanguage> GetLanguageFeaturesDynamicData()
+        {
+            return new List<ProgrammingLanguage>();
+        }
+    }
+}
--- ProgrammingLanguageDevelopment/ProgrammingLanguage.cs
+++ ProgrammingLanguageDevelopment/ProgrammingLanguage.cs
@@ -8,9 +8,39 @@ namespace ProgrammingLanguageDevelopment
 {
     class ProgrammingLanguage
     {
+        public string Name;
         public int Year;            //first appeared
-        public string Paradigm;     //imperative, structured, object-oriented or multi-paradigm 
-        public string Typing;       //typing discipline: static or dynamic
-        public string Level;        //high-level or low-level
+        public Paradigm _Paradigm;  //imperative, structured, object-oriented or multi-paradigm 
+        public Typing _Typing;       //typing discipline: static or dynamic
+        public Level _Level;        //high-level or low-level
+
+        public enum Paradigm
+        {
+            Imperative,
+            Structured,
+            Object_oriented,
+            Multi_paradigm
+        }
+
+        public enum Typing
+        {
+            Static,
+            Dynamic
+        }
+
+        public enum Level
+        {
+            Low,
+            High
+        }
+
+        public ProgrammingLanguage(string name, int year, Paradigm paradigm, Typing typing, Level level)
+        {
+            Name = name;
+            Year = year;
+            _Paradigm = paradigm;
+            _Typing = typing;
+            _Level = level;
+        }
     }
 }
