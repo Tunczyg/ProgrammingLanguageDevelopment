7f6e546c90f9d022454cf7adf07f5946bb276ae8
ilonatommy <ili.ili@interia.pl>
2019-04-28 19:54:40 +0200

    Parser for annual statistics of StackOverflow is nearly completed - waiting for AnnualStatisticData class. Added an ignored directory for private notes, etc.

--- ProgrammingLanguageDevelopment/WebParser.cs
+++ ProgrammingLanguageDevelopment/WebParser.cs
@@ -4,6 +4,8 @@ using System.Collections.Generic;
 using System.IO;
 using System.Net;
 using System.Text;
+using HtmlAgilityPack;
+using System.Linq;
 
 namespace ProgrammingLanguageDevelopment
 {
@@ -14,15 +16,59 @@ namespace ProgrammingLanguageDevelopment
         {
             for(var year = 2015; year <= DateTime.Today.Year; year++)
             {
-                var annualStats = GetRawSourceCode("https://insights.stackoverflow.com/survey/"+ year + "/");
-                //next step: search for valuable data in raw code and extract them into class
-                //might be useful (or not):
+                var html = GetRawSourceCode("https://insights.stackoverflow.com/survey/"+ year + "/");
+                var htmlDoc = new HtmlDocument();
+                htmlDoc.LoadHtml(html);
 
-                //serialise:
-                //string json = JsonConvert.SerializeObject(T);
+                var searchingPhrase = "";
+                switch(year)
+                {
+                    case 2015:
+                        {
+                            searchingPhrase = "techLanguages-" + year;
+                            break;
+                        }
+                    case 2016:
+                        {
+                            searchingPhrase = "technology-most-popular-technologies-" + year;
+                            break;
+                        }
+                    case 2017:
+                        {
+                            searchingPhrase = "technology-most-popular-technologies--of-this-category";
+                            break;
+                        }
+                    default:
+                        {
+                            searchingPhrase = "technology-most-popular-technologies-all-respondents";
+                            break;
+                        }
+                }
 
-                //deserialise:
-                //JsonConvert.DeserializeObject<T>(json);
+                var inputs = from items in htmlDoc.DocumentNode.Descendants("div")
+                                         where items.Attributes["id"] != null
+                                          && items.Attributes["id"].Value == searchingPhrase
+                             select items;
+                foreach (var input in inputs)
+                {
+                    var languages = from items in input.ChildNodes.Descendants("div")
+                                    where items.Attributes["class"] != null
+                                    && items.Attributes["class"].Value == "bar-row"
+                                    select items;
+
+                    //following code requires minor changes after creation of AnnualStatisticData
+                    var futureAnnualStatisticDataList = languages.Select(item => item.InnerText.Split(null).Where(part => part != "")).ToList();
+                    {
+                        //another (more convenient) option:
+                        //following code won't work until AnnualStatisticData creation and 
+                        //addition of [JsonProperty("$property_name")] before its fields
+                        //I want to convert: [JsonProperty("$bar-label")] (language name) and [JsonProperty("$lang-language_name")] 
+
+                        //var languagesHtml = languages.Select(language => language.InnerHtml.ToString()).ToList();
+                        //foreach (var language in languagesHtml)
+                        //JsonConvert.DeserializeObject<AnnualStatisticData>(language);
+                    }
+                }
             }
             return new List<AnnualStatisticData>();
         }
