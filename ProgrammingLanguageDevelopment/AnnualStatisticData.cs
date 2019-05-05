03720ada2618b1747f585f2dfb70d5a7a0119450
Wojciech Tomasiewicz <wojtarek@vp.pl>
2019-05-05 21:04:05 +0200

    Annual Statistic Data
    Added annual statistic data class filled with necessary fields.

---
+++ ProgrammingLanguageDevelopment/AnnualStatisticData.cs
@@ -0,0 +1,19 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Text;
+using HtmlAgilityPack;
+using System.Linq;
+
+namespace ProgrammingLanguageDevelopment
+{
+    class AnnualStatisticData
+    {
+        private int year;
+        private float tiobeIndex;
+        private int pullRequestsAmount;
+        private int publicationsAmount;
+    }
+}
