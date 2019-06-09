4d7f517ac2e478f03e458b5b05333b65fd0c5cdf
patrycjaszubart <patrycjaszubart@gmail.com>
2019-06-09 17:45:48 +0200

    catching errors

--- ARMA/Test/iteracja.m
+++ ARMA/Test/iteracja.m
@@ -7,6 +7,7 @@ btab(:,:) = struct2cell(tab(:));
 ctab = btab';
 s=size(ctab(:,1)); s = s(1);
 
+invalid = [];
 names = string(ctab(:,1));
 for i = 1:s-1
     for j = i+1:s
@@ -19,5 +20,10 @@ end
 names(names == " ") = []; %names without repeating
 
 for i = 1:length(names)
+    try
         Future_Prediction(names(i), 0.15, 5, names(i)+'.jpg',0.33, 0.33, 0.33);
+    catch
+        invalid = [invalid; names(i)];
+        continue
+    end
 end
